631c613318ddbe0c5cd3bfa2716421180d8becd7
ting <shaoshao613@gmail.com>
2015-07-24 14:03:41 +0800

    加上群间转发功能 删去消息字样更新

--- wx.js
+++ wx.js
@@ -1,11 +1,14 @@
 function saveMessage() {
-    pre=$("div.info:contains(‘百脑汇')>.ng-scope >.ng-binding").text();//获取右侧边栏的文字
+    pre=$("div.info:contains('百脑汇')>.ng-scope >.ng-binding").text();//获取右侧边栏的文字
     if(pre!=name){//如文字无变化，不执行
          name=pre;
          texts=name.split(':');
+        //群间相互转发，暂不可用
+        //$('#editArea').text(name)
+        //$('.btn_send').click()
          if(texts.length>1){//判断是否为我发出的消息
              user=texts[0];
-             if(user.split(']').length>0){
+             if(user.split(']').length>1){
                  user=user.split(']')[1];//取消［5条消息］字样
              }
              text=texts[1];
