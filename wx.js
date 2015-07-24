4a911c977adaf5d864a61cc37528b96280b3dbb8
ting <shaoshao613@gmail.com>
2015-07-24 13:50:29 +0800

    第一版

---
+++ wx.js
@@ -0,0 +1,33 @@
+function saveMessage() {
+    pre=$("div.info:contains(‘百脑汇')>.ng-scope >.ng-binding").text();//获取右侧边栏的文字
+    if(pre!=name){//如文字无变化，不执行
+         name=pre;
+         texts=name.split(':');
+         if(texts.length>1){//判断是否为我发出的消息
+             user=texts[0];
+             if(user.split(']').length>0){
+                 user=user.split(']')[1];//取消［5条消息］字样
+             }
+             text=texts[1];
+         }else{
+             user='我';
+             text=name;
+         }
+         $.ajax({
+                url: "https://tshao.sinaapp.com/test/wx.php",//发送数据到服务器
+                jsonp: "callback",
+                type: 'get',
+                dataType: "jsonp",
+                data: {
+                    user: user,
+                    text: text
+                },
+                success: function( response ) {
+                    console.log( response ); // server response
+                }
+        });
+
+    }
+    setTimeout("saveMessage()", 1);//每一毫秒执行一次
+}
+saveMessage();
